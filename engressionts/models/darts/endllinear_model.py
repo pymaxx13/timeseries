389bc8e8fd18e5a143dc19c974e2154fb5833f53
Rajdeep Pathak <pathakrajdeep91@gmail.com>
2026-08-09 13:16:08 +0400

    Fix unpacking of x_in to limit to first three elements

--- engressionts/models/darts/endllinear_model.py
+++ engressionts/models/darts/endllinear_model.py
@@ -165,7 +165,7 @@ class _EnDLinearModule(EngressionPLModule):
             `x_future` is the output/future chunk. Input dimensions are `(n_samples, n_time_steps, n_variables)`
         """
 
-        x, x_future, x_static = x_in  # x: (batch, in_len, in_dim)
+        x, x_future, x_static = x_in[:3]  # x: (batch, in_len, in_dim)
         x = self.noise_layer(x)
         batch, _, _ = x.shape
 
